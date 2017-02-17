861b362d6126405d30bfe4213ccfd7c782c14dc4
No-Jun Park <npark@mozilla.com>
2017-02-16 23:32:20 -0500

    retest

---
+++ app/src/androidTest/java/org/mozilla/focus/activity/MainActivityTest.java
@@ -0,0 +1,38 @@
+package org.mozilla.focus.activity;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityTest() {
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withId(org.mozilla.focus.R.id.settings),
+                        withParent(allOf(withId(org.mozilla.focus.R.id.activity_main),
+                                withParent(withId(org.mozilla.focus.R.id.container)))),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+
+    }
+
+}
