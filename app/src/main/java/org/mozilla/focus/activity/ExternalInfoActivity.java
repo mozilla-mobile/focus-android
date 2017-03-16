17e1f6a85d5cdceae80456ed49a9abe8a9e102fa
Andrzej Hunt <andrzej@ahunt.org>
2017-03-16 14:43:32 +0100

    Introduce ExternalInfoActivity to show manifesto in a custom tab (#199)
    More pages will be added soon.

---
+++ app/src/main/java/org/mozilla/focus/activity/ExternalInfoActivity.java
@@ -0,0 +1,38 @@
+package org.mozilla.focus.activity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.customtabs.CustomTabsIntent;
+
+import org.mozilla.focus.customtabs.CustomTabsHelper;
+import org.mozilla.focus.utils.SupportUtils;
+
+/**
+ * This is not an actual activity - this is just a helper to show an information page in a new
+ * activity.
+ */
+public class ExternalInfoActivity {
+
+    private static void launchURL(final Context context, final String url) {
+        CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+
+        CustomTabsIntent customTabIntent = builder.build();
+
+        final String ctPackage = CustomTabsHelper.getPackageNameToUse(context);
+
+        if (ctPackage != null) {
+            customTabIntent.intent.setPackage(ctPackage);
+            customTabIntent.launchUrl(context, Uri.parse(url));
+        } else {
+            // Fallback to InfoActivity if there's really no custom tabs support in existence.
+            final Intent intent = InfoActivity.getIntentFor(context, url, "");
+            context.startActivity(intent);
+        }
+    }
+
+    public static void launchManifesto(final Context context) {
+        final String manifestoURL = SupportUtils.getManifestoURL();
+        launchURL(context, manifestoURL);
+    }
+}
