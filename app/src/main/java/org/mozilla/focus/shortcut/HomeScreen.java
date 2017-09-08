63d7a4a1973cc6bf46a6b171100d2278cdcec46f
Emily Kager <ekager@mills.edu>
2017-09-08 16:08:20 -0700

    Homescreen shortcut remembering the blocker switch status (#1091)

--- app/src/main/java/org/mozilla/focus/shortcut/HomeScreen.java
+++ app/src/main/java/org/mozilla/focus/shortcut/HomeScreen.java
@@ -21,16 +21,17 @@ import org.mozilla.focus.utils.UrlUtils;
 
 public class HomeScreen {
     public static final String ADD_TO_HOMESCREEN_TAG = "add_to_homescreen";
+    public static final String BLOCKING_ENABLED = "blocking_enabled";
 
     /**
      * Create a shortcut for the given website on the device's home screen.
      */
-    public static void installShortCut(Context context, Bitmap icon, String url, String title) {
+    public static void installShortCut(Context context, Bitmap icon, String url, String title, boolean blockingEnabled) {
         if (TextUtils.isEmpty(title.trim())) {
             title = generateTitleFromUrl(url);
         }
 
-        installShortCutViaManager(context, icon, url, title);
+        installShortCutViaManager(context, icon, url, title, blockingEnabled);
 
         // Creating shortcut flow is different on Android up to 7, so we want to go
         // to the home screen manually where the user will see the new shortcut appear
@@ -47,22 +48,23 @@ public class HomeScreen {
      * On Android 8+ the user will have the ability to add the shortcut manually
      * or let the system place it automatically.
      */
-    private static void installShortCutViaManager(Context context, Bitmap bitmap, String url, String title) {
+    private static void installShortCutViaManager(Context context, Bitmap bitmap, String url, String title, boolean blockingEnabled) {
         if (ShortcutManagerCompat.isRequestPinShortcutSupported(context)) {
             final ShortcutInfoCompat shortcut = new ShortcutInfoCompat.Builder(context, url)
                     .setShortLabel(title)
                     .setLongLabel(title)
                     .setIcon(IconCompat.createWithBitmap(bitmap))
-                    .setIntent(createShortcutIntent(context, url))
+                    .setIntent(createShortcutIntent(context, url, blockingEnabled))
                     .build();
             ShortcutManagerCompat.requestPinShortcut(context, shortcut, null);
         }
     }
 
-    private static Intent createShortcutIntent(Context context, String url) {
+    private static Intent createShortcutIntent(Context context, String url, boolean blockingEnabled) {
         final Intent shortcutIntent = new Intent(context, MainActivity.class);
         shortcutIntent.setAction(Intent.ACTION_VIEW);
         shortcutIntent.setData(Uri.parse(url));
+        shortcutIntent.putExtra(BLOCKING_ENABLED, blockingEnabled);
         shortcutIntent.putExtra(ADD_TO_HOMESCREEN_TAG, ADD_TO_HOMESCREEN_TAG);
         return shortcutIntent;
     }
