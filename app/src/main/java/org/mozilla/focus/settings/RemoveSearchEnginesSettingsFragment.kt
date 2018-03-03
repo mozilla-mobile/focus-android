1c7dda76b222c45dd13f450acd75d8c76aff2370
Markus Hintersteiner <m.hintersteiner@gmail.com>
2018-03-03 13:04:59 +0100

    Fix style errors reported by detekt

--- app/src/main/java/org/mozilla/focus/settings/RemoveSearchEnginesSettingsFragment.kt
+++ app/src/main/java/org/mozilla/focus/settings/RemoveSearchEnginesSettingsFragment.kt
@@ -39,7 +39,10 @@ class RemoveSearchEnginesSettingsFragment : BaseSettingsFragment() {
     override fun onPrepareOptionsMenu(menu: Menu?) {
         super.onPrepareOptionsMenu(menu)
         view?.post({
-            val pref = preferenceScreen.findPreference(resources.getString(R.string.pref_key_multiselect_search_engine_list)) as MultiselectSearchEngineListPreference
+            val pref = preferenceScreen
+                .findPreference(resources.getString(R.string.pref_key_multiselect_search_engine_list))
+                as MultiselectSearchEngineListPreference
+
             menu?.findItem(R.id.menu_delete_items)?.let {
                 ViewUtils.setMenuItemEnabled(it, pref.atLeastOneEngineChecked())
             }
@@ -49,7 +52,9 @@ class RemoveSearchEnginesSettingsFragment : BaseSettingsFragment() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.menu_delete_items -> {
-                val pref = preferenceScreen.findPreference(resources.getString(R.string.pref_key_multiselect_search_engine_list))
+                val pref = preferenceScreen
+                    .findPreference(resources.getString(R.string.pref_key_multiselect_search_engine_list))
+
                 val enginesToRemove = (pref as MultiselectSearchEngineListPreference).checkedEngineIds
                 TelemetryWrapper.removeSearchEnginesEvent(enginesToRemove.size)
                 SearchEngineManager.removeSearchEngines(enginesToRemove, getSearchEngineSharedPreferences())
