14fd5b5d9c78de224c8edf2309c8ca4afa0b1b9d
Fernando Garcia Alvarez <fercarcedo@gmail.com>
2018-03-08 19:19:41 +0100

    Add license and remove comment

--- app/src/main/java/org/mozilla/focus/utils/StatusBarUtils.java
+++ app/src/main/java/org/mozilla/focus/utils/StatusBarUtils.java
@@ -1,11 +1,11 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 package org.mozilla.focus.utils;
 
 import android.view.View;
 import android.view.WindowInsets;
-
-/**
- * Created by Fer on 08/03/2018.
- */
 
 public class StatusBarUtils {
     private static int STATUS_BAR_SIZE = -1;
