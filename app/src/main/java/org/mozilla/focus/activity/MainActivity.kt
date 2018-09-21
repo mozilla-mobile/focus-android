752c161b5dce65e351330af5326703b8cb7c2652
Colin Lee <mncolinlee@gmail.com>
2018-09-21 16:54:11 -0500

    Issue #3443: Figure out Why GV A/B Experiment Isn't Rolling Out

--- app/src/main/java/org/mozilla/focus/activity/MainActivity.kt
+++ app/src/main/java/org/mozilla/focus/activity/MainActivity.kt
@@ -28,6 +28,7 @@ import org.mozilla.focus.settings.ExperimentsSettingsFragment
 import org.mozilla.focus.telemetry.SentryWrapper
 import org.mozilla.focus.telemetry.TelemetryWrapper
 import org.mozilla.focus.utils.AppConstants
+import org.mozilla.focus.utils.ExperimentsSyncService
 import org.mozilla.focus.utils.Settings
 import org.mozilla.focus.utils.ViewUtils
 import org.mozilla.focus.viewmodel.MainViewModel
@@ -164,6 +165,7 @@ open class MainActivity : LocaleAwareAppCompatActivity() {
         super.onStop()
 
         TelemetryWrapper.stopMainActivity()
+        ExperimentsSyncService.scheduleSync(this)
     }
 
     override fun onNewIntent(unsafeIntent: Intent) {
@@ -316,5 +318,7 @@ open class MainActivity : LocaleAwareAppCompatActivity() {
         const val EXTRA_NOTIFICATION = "notification"
 
         private const val EXTRA_SHORTCUT = "shortcut"
+
+        const val EXPERIMENTS_JOB_ID: Int = 4141
     }
 }
