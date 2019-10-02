1f9c0d16b4609247ac8c9053af71fbe21ec64745
rohitverma1999hack <rohitvermaavcbbk2011@gmail.com>
2019-10-02 21:20:40 +0530

    Replace switch with checkbox

--- app/src/main/java/org/mozilla/focus/menu/browser/RequestDesktopCheckItemViewHolder.kt
+++ app/src/main/java/org/mozilla/focus/menu/browser/RequestDesktopCheckItemViewHolder.kt
@@ -5,8 +5,8 @@
 package org.mozilla.focus.menu.browser
 
 import android.view.View
-import android.widget.CheckBox
 import android.widget.CompoundButton
+import android.widget.Switch
 
 import org.mozilla.focus.R
 import org.mozilla.focus.fragment.BrowserFragment
@@ -20,18 +20,18 @@ internal class RequestDesktopCheckItemViewHolder/* package */(
     itemView: View,
     private val fragment: BrowserFragment
 ) : BrowserMenuViewHolder(itemView), CompoundButton.OnCheckedChangeListener {
-    private val checkbox: CheckBox = itemView.findViewById(R.id.check_menu_item_checkbox)
+    private val switch: Switch = itemView.findViewById(R.id.check_menu_item_checkbox)
 
     init {
-        checkbox.isChecked = fragment.session.shouldRequestDesktopSite
-        checkbox.setOnCheckedChangeListener(this)
+        switch.isChecked = fragment.session.shouldRequestDesktopSite
+        switch.setOnCheckedChangeListener(this)
     }
 
     override fun onCheckedChanged(buttonView: CompoundButton, isChecked: Boolean) {
         fragment.setShouldRequestDesktop(isChecked)
         TelemetryWrapper.desktopRequestCheckEvent(isChecked)
 
-        // Delay closing the menu and reloading the website a bit so that the user can actually see
+        // Delay closing the menu and reloading the website a bit so that the user can actuallgity see
         // the switch change its state.
         ThreadUtils.postToMainThreadDelayed(Runnable {
             menu.dismiss()
