988c853eca4b7321dec18668476320f03ee97e54
yuewenma <mayuewen97@gmail.com>
2020-03-18 21:49:46 -0400

    fixed import conflict issue removed log fixed magic number

--- app/src/main/java/org/mozilla/focus/fragment/BrowserFragment.kt
+++ app/src/main/java/org/mozilla/focus/fragment/BrowserFragment.kt
@@ -483,9 +483,8 @@ class BrowserFragment : WebFragment(), LifecycleObserver, View.OnClickListener,
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         when (requestCode) {
-            9999 -> {
-                Log.i("Test", "Result URI " + data!!.data)
-                val uri = data.data
+            REQUEST_CODE -> {
+                val uri = data!!.data
                 val docUri = DocumentsContract.buildDocumentUriUsingTree(uri,
                         DocumentsContract.getTreeDocumentId(uri))
                 var path = docUri.path
@@ -589,9 +588,6 @@ class BrowserFragment : WebFragment(), LifecycleObserver, View.OnClickListener,
             }
 
             override fun onDownloadStart(download: Download) {
-                val i = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE)
-                i.addCategory(Intent.CATEGORY_DEFAULT)
-                startActivityForResult(Intent.createChooser(i, "Choose directory"), 9999)
                 if (PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(
                         requireContext(),
                         Manifest.permission.WRITE_EXTERNAL_STORAGE
@@ -599,6 +595,9 @@ class BrowserFragment : WebFragment(), LifecycleObserver, View.OnClickListener,
                 ) {
                     // Long press image displays its own dialog and we handle other download cases here
                     if (!isDownloadFromLongPressImage(download)) {
+                        val i = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE)
+                        i.addCategory(Intent.CATEGORY_DEFAULT)
+                        startActivityForResult(Intent.createChooser(i, "Choose directory"), REQUEST_CODE)
                         showDownloadPromptDialog(download)
                     } else {
                         // Download dialog has already been shown from long press on image. Proceed with download.
@@ -1526,6 +1525,8 @@ class BrowserFragment : WebFragment(), LifecycleObserver, View.OnClickListener,
         private const val RESTORE_KEY_DOWNLOAD = "download"
 
         private const val INITIAL_PROGRESS = 5
+
+        private const val REQUEST_CODE = 9999
 
         @JvmStatic
         fun createForSession(session: Session): BrowserFragment {
