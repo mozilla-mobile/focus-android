60c981e7b35880e60ab91732e4e39bbede521db6
Sebastian Kaspari <s.kaspari@gmail.com>
2020-06-05 12:15:35 +0200

    Clean up EngineProvider class.

--- app/src/main/java/org/mozilla/focus/components/EngineProvider.kt
+++ app/src/main/java/org/mozilla/focus/components/EngineProvider.kt
@@ -1,18 +1,18 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 package org.mozilla.focus.components
 
 import android.content.Context
-import android.util.AttributeSet
-import android.util.Log
 import mozilla.components.browser.engine.gecko.GeckoEngine
-import mozilla.components.concept.engine.*
-import mozilla.components.concept.engine.utils.EngineVersion
+import mozilla.components.concept.engine.DefaultSettings
+import mozilla.components.concept.engine.Engine
 import mozilla.components.lib.crash.handler.CrashHandlerService
-import org.json.JSONObject
 import org.mozilla.geckoview.GeckoRuntime
 import org.mozilla.geckoview.GeckoRuntimeSettings
 
 object EngineProvider {
-
     private var runtime: GeckoRuntime? = null
 
     @Synchronized
@@ -32,47 +32,6 @@ object EngineProvider {
     fun createEngine(context: Context, defaultSettings: DefaultSettings): Engine {
         val runtime = getOrCreateRuntime(context)
 
-        return EngineWrapper(GeckoEngine(context, defaultSettings, runtime).also {
-            // WebCompatFeature.install(it)
-        })
+        return GeckoEngine(context, defaultSettings, runtime)
     }
-
-    /*
-    fun createClient(context: Context): Client {
-        val runtime = getOrCreateRuntime(context)
-        return GeckoViewFetchClient(context, runtime)
-    }
-     */
-}
-
-private class EngineWrapper(
-    private val actual: Engine
-) : Engine {
-    override val settings: Settings
-        get() = actual.settings
-
-    override val version: EngineVersion
-        get() = actual.version
-
-    override fun createSession(private: Boolean, contextId: String?): EngineSession {
-        return actual.createSession(private, contextId)
-    }
-
-    override fun createSessionState(json: JSONObject): EngineSessionState {
-        return actual.createSessionState(json)
-    }
-
-    override fun createView(context: Context, attrs: AttributeSet?): EngineView {
-        return actual.createView(context, attrs)
-    }
-
-    override fun name(): String {
-        return actual.name()
-    }
-
-    override fun speculativeConnect(url: String) {
-        Log.w("SKDBG", "Speculative connect: $url")
-        return actual.speculativeConnect(url)
-    }
-
 }
