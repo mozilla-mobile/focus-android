9eb13445ddb00f279bb4b984f2c71ba555429151
Sebastian Kaspari <s.kaspari@gmail.com>
2020-06-17 14:48:39 +0200

    Issue #4553: Integrate feature-downloads component.

--- app/src/main/java/org/mozilla/focus/Components.kt
+++ app/src/main/java/org/mozilla/focus/Components.kt
@@ -13,12 +13,16 @@ import mozilla.components.browser.state.store.BrowserStore
 import mozilla.components.concept.engine.DefaultSettings
 import mozilla.components.concept.engine.Engine
 import mozilla.components.concept.engine.EngineSession
+import mozilla.components.concept.fetch.Client
 import mozilla.components.feature.contextmenu.ContextMenuUseCases
+import mozilla.components.feature.downloads.DownloadMiddleware
+import mozilla.components.feature.downloads.DownloadsUseCases
 import mozilla.components.feature.session.SessionUseCases
 import mozilla.components.feature.session.SettingsUseCases
 import mozilla.components.feature.session.TrackingProtectionUseCases
 import mozilla.components.feature.tabs.TabsUseCases
 import org.mozilla.focus.components.EngineProvider
+import org.mozilla.focus.downloads.DownloadService
 import org.mozilla.focus.engine.LocalizedContentInterceptor
 import org.mozilla.focus.search.BingSearchEngineFilter
 import org.mozilla.focus.search.CustomSearchEngineProvider
@@ -48,17 +52,27 @@ class Components(
         }
     }
 
+    val client: Client by lazy { EngineProvider.createClient(context) }
+
     val trackingProtectionUseCases by lazy { TrackingProtectionUseCases(sessionManager, engine) }
 
     val settingsUseCases by lazy { SettingsUseCases(engine.settings, sessionManager) }
 
-    val store: BrowserStore by lazy { BrowserStore() }
+    val store: BrowserStore by lazy {
+        BrowserStore(
+            middleware = listOf(
+                DownloadMiddleware(context, DownloadService::class.java)
+            )
+        )
+    }
 
     val sessionUseCases: SessionUseCases by lazy { SessionUseCases(sessionManager) }
 
     val tabsUseCases: TabsUseCases by lazy { TabsUseCases(sessionManager) }
 
     val contextMenuUseCases: ContextMenuUseCases by lazy { ContextMenuUseCases(sessionManager, store) }
+
+    val downloadsUseCases: DownloadsUseCases by lazy { DownloadsUseCases(store) }
 
     val sessionManager by lazy {
         SessionManager(engine, store)
