b3289aad3b636b411c53fe1c12dbd01005bc0dc6
Gouri Panda <gouripanda4@gmail.com>
2021-05-07 18:33:27 -0700

    restores edittext and it's position in add search engine
    changed as requested
    
    detekt fix
    
    Changed as requested
    
    lint fix

--- app/src/main/java/org/mozilla/focus/search/ManualAddSearchEnginePreference.kt
+++ app/src/main/java/org/mozilla/focus/search/ManualAddSearchEnginePreference.kt
@@ -4,12 +4,10 @@
 
 package org.mozilla.focus.search
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
 import android.os.Parcelable
-import com.google.android.material.textfield.TextInputLayout
-import androidx.preference.Preference
-import androidx.preference.PreferenceViewHolder
 import android.text.Editable
 import android.text.TextUtils
 import android.text.TextWatcher
@@ -17,10 +15,13 @@ import android.util.AttributeSet
 import android.view.View
 import android.widget.EditText
 import android.widget.ProgressBar
+import androidx.preference.Preference
+import androidx.preference.PreferenceViewHolder
+import com.google.android.material.textfield.TextInputLayout
 import org.mozilla.focus.R
 import org.mozilla.focus.utils.UrlUtils
 import org.mozilla.focus.utils.ViewUtils
-
+@Suppress("TooManyFunctions")
 class ManualAddSearchEnginePreference(context: Context, attrs: AttributeSet) :
     Preference(context, attrs) {
     private var engineNameEditText: EditText? = null
@@ -32,7 +33,10 @@ class ManualAddSearchEnginePreference(context: Context, attrs: AttributeSet) :
 
     private var savedSearchEngineName: String? = null
     private var savedSearchQuery: String? = null
+    private var lastEditTextTouched: String? = null
+    private var cursorPosition: Int? = null
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onBindViewHolder(holder: PreferenceViewHolder?) {
         super.onBindViewHolder(holder)
 
@@ -57,8 +61,17 @@ class ManualAddSearchEnginePreference(context: Context, attrs: AttributeSet) :
         progressView = holder.findViewById(R.id.progress) as ProgressBar
 
         updateState()
+        if (lastEditTextTouched == null) {
+            ViewUtils.showKeyboard(engineNameEditText)
+        }
+    }
 
-        ViewUtils.showKeyboard(engineNameEditText)
+    private fun saveEditText() {
+        if (engineNameEditText?.isFocused == true) {
+            lastEditTextTouched = SEARCH_ENGINE_NAME_KEY
+        } else if (searchQueryEditText?.isFocused == true) {
+            lastEditTextTouched = SEARCH_QUERY_KEY
+        }
     }
 
     override fun onRestoreInstanceState(state: Parcelable?) {
@@ -66,16 +79,26 @@ class ManualAddSearchEnginePreference(context: Context, attrs: AttributeSet) :
         super.onRestoreInstanceState(bundle.getParcelable(SUPER_STATE_KEY))
         savedSearchEngineName = bundle.getString(SEARCH_ENGINE_NAME_KEY)
         savedSearchQuery = bundle.getString(SEARCH_QUERY_KEY)
+        lastEditTextTouched = bundle.getString(LASTEDITTEXT)
+        cursorPosition = bundle.getString(CURSOR_POSITION)?.toInt()
     }
 
     override fun onSaveInstanceState(): Parcelable {
         val state = super.onSaveInstanceState()
+        saveEditText()
         val bundle = Bundle()
         bundle.putParcelable(SUPER_STATE_KEY, state)
         bundle.putString(SEARCH_ENGINE_NAME_KEY, engineNameEditText?.text.toString())
         bundle.putString(SEARCH_QUERY_KEY, searchQueryEditText?.text.toString())
-
+        bundle.putString(LASTEDITTEXT, lastEditTextTouched)
+        bundle.putString(CURSOR_POSITION, getCursorPosition(lastEditTextTouched))
         return bundle
+    }
+
+    private fun getCursorPosition(lastEditTxtSelection: String?): String? = when (lastEditTxtSelection) {
+        SEARCH_ENGINE_NAME_KEY -> engineNameEditText?.selectionStart.toString()
+        SEARCH_QUERY_KEY -> searchQueryEditText?.selectionStart.toString()
+        else -> (-1).toString()
     }
 
     fun validateEngineNameAndShowError(engineName: String): Boolean {
@@ -112,6 +135,23 @@ class ManualAddSearchEnginePreference(context: Context, attrs: AttributeSet) :
     private fun updateState() {
         if (engineNameEditText != null) engineNameEditText?.setText(savedSearchEngineName)
         if (searchQueryEditText != null) searchQueryEditText?.setText(savedSearchQuery)
+        if (lastEditTextTouched != null) {
+            if (lastEditTextTouched == SEARCH_ENGINE_NAME_KEY) {
+                engineNameEditText?.requestFocus()
+                if (engineNameEditText != null && cursorPosition != null) {
+                    setCursorPosition(engineNameEditText!!, cursorPosition!!)
+                }
+            } else if (lastEditTextTouched == SEARCH_QUERY_KEY) {
+                searchQueryEditText?.requestFocus()
+                if (searchQueryEditText != null && cursorPosition != null) {
+                    setCursorPosition(searchQueryEditText!!, cursorPosition!!)
+                }
+            }
+        }
+    }
+
+    private fun setCursorPosition(editText: EditText, position: Int) {
+        editText.setSelection(position)
     }
 
     private fun buildTextWatcherForErrorLayout(errorLayout: TextInputLayout): TextWatcher {
@@ -130,5 +170,7 @@ class ManualAddSearchEnginePreference(context: Context, attrs: AttributeSet) :
         private val SUPER_STATE_KEY = "super-state"
         private val SEARCH_ENGINE_NAME_KEY = "search-engine-name"
         private val SEARCH_QUERY_KEY = "search-query"
+        private const val LASTEDITTEXT = "save-edit-text"
+        private const val CURSOR_POSITION = "cursor-position"
     }
 }
