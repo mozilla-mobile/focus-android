3e6dbd7d0122c156ae9d0a3c7ef0b90983ac967f
Ionut Cristian Bedregeanu <ionut.bedregeanu@softvision.ro>
2021-09-03 13:07:24 +0300

    For #5247 - Restore the position of find in page bar
    When the toolbar it will be moved to the bottom of the screen then we should
    move also the Find in page bar to the bottom.
    Until then we should let the Find in page bar at the top of the screen, over toolbar

--- app/src/main/java/org/mozilla/focus/fragment/BrowserFragment.kt
+++ app/src/main/java/org/mozilla/focus/fragment/BrowserFragment.kt
@@ -21,7 +21,6 @@ import android.view.accessibility.AccessibilityManager
 import android.webkit.MimeTypeMap
 import android.widget.FrameLayout
 import android.widget.Toast
-import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.content.ContextCompat
 import androidx.preference.PreferenceManager
 import com.google.android.material.appbar.AppBarLayout
@@ -47,7 +46,6 @@ import mozilla.components.feature.downloads.AbstractFetchDownloadService
 import mozilla.components.feature.downloads.DownloadsFeature
 import mozilla.components.feature.downloads.manager.FetchDownloadManager
 import mozilla.components.feature.downloads.share.ShareDownloadFeature
-import mozilla.components.feature.findinpage.view.FindInPageBar
 import mozilla.components.feature.prompts.PromptFeature
 import mozilla.components.feature.session.SessionFeature
 import mozilla.components.feature.tabs.WindowFeature
@@ -285,7 +283,6 @@ class BrowserFragment :
         )
 
         customizeToolbar(view)
-        customizeFindInPage(view)
 
         val customTabConfig = tab.ifCustomTab()?.config
         if (customTabConfig != null) {
@@ -317,13 +314,6 @@ class BrowserFragment :
         } else {
             null
         }
-    }
-
-    private fun customizeFindInPage(view: View) {
-        val findInPageBar = view.findViewById<FindInPageBar>(R.id.find_in_page)
-        val newParams = findInPageBar.layoutParams as CoordinatorLayout.LayoutParams
-        newParams.gravity = Gravity.BOTTOM
-        findInPageBar.layoutParams = newParams
     }
 
     private fun customizeToolbar(view: View) {
