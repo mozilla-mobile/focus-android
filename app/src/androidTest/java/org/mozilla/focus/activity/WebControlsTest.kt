42e89206c187a58e8cdd5bc2247c14d33069b2e7
AndiAJ <andiaj@users.noreply.github.com>
2022-04-26 10:56:51 +0300

    For #6925 disable failing verifyCalendarFormTest UI test

--- app/src/androidTest/java/org/mozilla/focus/activity/WebControlsTest.kt
+++ app/src/androidTest/java/org/mozilla/focus/activity/WebControlsTest.kt
@@ -7,6 +7,7 @@ import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.After
 import org.junit.Before
+import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -154,6 +155,7 @@ class WebControlsTest {
         }
     }
 
+    @Ignore("Failing , see https://github.com/mozilla-mobile/focus-android/issues/6925")
     @SmokeTest
     @Test
     fun verifyCalendarFormTest() {
